6340fa0d9e44d2c37d7bbd16c913f4b1c04f190e
Arturo Molina <arturomolinacorrea02@gmail.com>
2026-04-17 18:36:25 -0500

    Añadido el método visualizar y arreglados problemas de indentación de la clase Grafos

--- grafos.py
+++ grafos.py
@@ -1,9 +1,16 @@
 from itertools import combinations
 from logica import *
 import matplotlib.pyplot as plt
+import networkx as nx
 import matplotlib.patches as patches
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 from types import MethodType 
+
+
+
+
+
+
 
 def escribir_grafos(self, literal):
     if '-' in literal:
@@ -29,46 +36,111 @@ class Grafos:
     que conecte el nodo H con el nodo B bajo
     la restricción de recorrer cada arista exactamente una vez'''
 
-def __init__(self): 
+    def __init__(self): 
         # Definición de las aristas del grafo (no dirigidas)
-        aristas_base = [ 
+        self.aristas_base = [ 
         ('H','G'),('H','B'),('H','E'),('G','A'),('A','B'),('B','C'),('C','D'),('D','E')
         ] 
         recorridos = []
-        for u, v in aristas_base:
+        for u, v in self.aristas_base:
             recorridos.append((u, v))
             recorridos.append((v, u)) 
         turnos = 8
         
         self.To = Descriptor([recorridos, turnos])
-        self.To.escribir = MethodType(escribir_grafos, self.CenC)
+        self.To.escribir = MethodType(escribir_grafos, self.To)
         r1 = self.regla1()
         r2 = self.regla2()
         r3 = self.regla3()
         self.reglas = [r1, r2, r3] 
 
-        def regla1(self): 
-            #TODO
-            pass 
+    def regla1(self): 
+        #TODO
+        return "Por hacer"
 
-        def regla2(self):
-             #TODO 
-             pass  
-        
-        def regla3(self): 
-            #TODO
-            pass 
+    def regla2(self):
+        #TODO 
+        return "Por hacer"  
+    
+    def regla3(self): 
+        #TODO
+        return "Por hacer"  
 
-        def regla4(self):
-             #TODO 
-             pass   
-        def regla5(self):
-             #TODO 
-             pass  
+    def regla4(self):
+        #TODO 
+        return "Por hacer"    
+    def regla5(self):
+        #TODO 
+        return "Por hacer"   
+    
+    #ESTA ES UNA FUNCIÓN AUXILIAR PARA VISUALIZAR QUE DEVUELVE UNA TRIPLA ("X","Y",t)
+    def triple_conversion(self, atomo): 
+        p, t = self.To.unravel(atomo)   
+        recorridos = self.To.args_lista[0]
+        tupla = recorridos[p]
+        return (tupla[0], tupla[1], t)
+    
+
+    def visualizar_grafo(self, dict_atomos=""):
+        #Creamos una copia para asegurarnos que no pase 
+        copia_dict_atomos = dict_atomos.copy()
+        G = nx.Graph()        
+        G.add_edges_from(self.aristas_base)    
+        pos = nx.spring_layout(G, seed=6)
+    
+        plt.figure(figsize=(5, 6))
+        nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=1000)
+        nx.draw_networkx_labels(G, pos, font_weight='bold')
+        nx.draw_networkx_edges(G, pos, width=2, edge_color='lightgray')
+        movement_list = []
+
+        if copia_dict_atomos: # 
+            #IMPORTANTE: DEL DICCIONARIO SOLO TOMAMOS AQUELLOS VALORES QUE SON VERDADEROS
+            for atomo in dict_atomos: 
+                if copia_dict_atomos[atomo] == True:  
+                    #Decodifica el atomo y lo vuelve una tripla
+                    triple = self.triple_conversion(atomo)  
+                    print(triple)
+                    movement_list.append(triple)
+            print(movement_list)
+
+            propiedades_caja = dict(
+                boxstyle='round,pad=0.3',
+                edgecolor='red',          
+                facecolor='white',        
+                alpha=0.9                
+            )
+
+            for triple in movement_list:  
+                v1, v2, turno = triple # Desempaquetado rápido de la tupla
+            
+                # CORRECCIÓN AQUÍ: G y pos van primero.
+                # Eliminamos v1, v2 y turno de los argumentos porque no existen en la función.
+                nx.draw_networkx_edges(
+                    G,
+                    pos,
+                    edgelist=[(v1, v2)],
+                    edge_color='red',
+                    width=4,
+                    arrows=True,
+                    arrowstyle='->',
+                    arrowsize=30
+                )
+
+                nx.draw_networkx_edge_labels(
+                    G,
+                    pos,
+                    edge_labels={(v1, v2): f"Turno {turno}"},
+                    font_color='red',
+                    font_weight='bold',
+                    bbox=propiedades_caja
+                )
         
-        def visualizar(self): 
-             #TODO 
-             pass 
+        plt.title("Problema de grafos")
+        plt.axis('off')
+        plt.show()
+        
+
         
         
         
